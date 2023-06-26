3cfc8bc54e0aa084aeed80e056d1c6a38fdf343e
Mallory Van Wyngaarden <m.vanwyngaarden@gmail.com>
2023-06-26 09:16:45 -0300

    Updating some notes and consolidating some steps from the main QIIME pipeline script

--- code/rescript_createReferenceDB.sh
+++ code/rescript_createReferenceDB.sh
@@ -9,7 +9,7 @@ qiime rescript get-ncbi-data \
     --o-taxonomy fish-12S-ref-tax.qza \
     --verbose
     
-#Dereplicate reference data
+#Dereplicate reference data - sequences and taxonomy
 qiime rescript dereplicate \
  --i-sequences fish-12S-ref-seqs.qza \
  --i-taxa fish-12S-ref-tax.qza \
@@ -19,7 +19,7 @@ qiime rescript dereplicate \
  --o-dereplicated-sequences fish-12S-ref-seqs-derep.qza \
  --o-dereplicated-taxa fish-12S-ref-tax-derep.qza
  
- #filter low-quality sequences
+ #filter low-quality sequences and remove
  qiime rescript cull-seqs \
  --i-sequences fish-12S-ref-seqs-derep.qza \
  --p-n-jobs 20 \
@@ -32,8 +32,30 @@ qiime rescript dereplicate \
  --i-sequences fish-12S-ref-seqs-cull.qza \
  --p-global-min 90 \
  --p-global-max 1000 \
- --o-filtered-seqs fish-12S-ref-seqs-keep.qza \
+ --o-filtered-seqs fish-12S-ref-seqs-FINAL.qza \
  --o-discarded-seqs fish-12S-ref-seqs-discard.qza
+ 
+#filter the derep taxonomy to include only the seqs in the FINAL ref file
+qiime rescript filter-taxa \
+--i-taxonomy fish-12S-ref-tax-derep.qza \
+--m-ids-to-keep-file fish-12S-ref-seqs-FINAL.qza \
+--o-filtered-taxonomy fish-12S-ref-tax-FINAL.qza
+
+#visualize for evaluation
+qiime rescript evaluate-taxonomy \
+--i-taxonomies fish-12S-ref-tax-FINAL.qza \
+--o-taxonomy-stats fish-12S-ref-tax-FINAL-eval.qzv
+
+#tabulate and visualize output
+qiime metadata tabulate \
+--m-input-file fish-12S-ref-tax-FINAL.qza \
+--o-visualization fish-12S-ref-tax-FINAL.qzv &&
+qiime rescript evaluate-seqs \
+--i-sequences fish-12S-ref-seqs-FINAL.qza \
+--p-kmer-lengths 32 16 8 \
+--o-visualization fish-12S-ref-seqs-FINAL-eval.qzv
+ 
+
 
 #############################################
 #############Now do the same for 16S fish
