8389c839bc54a1d322f0e650da983d44f12a539a
NickJeff13 <Nick.w.jeffery3@gmail.com>
2024-02-19 10:59:29 -0400

    moving files

---
+++ code/Bioinformatics/cutadapt_primers2.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+#removing primers from the 5' end of both forward and reverse reads as illumina adapters have already been removed
+#Can tailor this to remove adapters or just one primer if only using forward reads
+for i in *_R1.fastq.gz;
+do
+  SAMPLE=$(echo ${i} | sed "s/_R1.fastq.gz//") 
+  echo ${SAMPLE}_R1.fastq.gz ${SAMPLE}_R2.fastq.gz
+  cutadapt -g GTCGGTAAAACTCGTGCCAGC \
+  -G NNNNNNCATAGTGGGGTATCTAATCCCAGTTTG \
+    --discard-untrimmed --nextseq-trim 20 -m 20 -n 2 \
+    --match-read-wildcards \
+    -o ${SAMPLE}_R1.fastq \
+    -p ${SAMPLE}_R2.fastq \
+    ${SAMPLE}_R1.fastq.gz  ${SAMPLE}_R2.fastq.gz ;
+done
+
+done
+qiime cutadapt trim-paired \
+--i-demultiplexed-sequences 16S-combined-demux.qza \
+--p-cores 40 \
+--p-anywhere-f AGCGYAATCACTTGTCTYTTAA \
+--p-anywhere-r CRBGGTCGCCCCAACCRAA \
+--p-error-rate 0.11 \
+--p-discard-untrimmed True \
+--p-match-read-wildcards \
+--p-match-adapter-wildcards \
+--o-trimmed-sequences 16s-demux-trimmed.qza \
+--output-dir  trimmed \
+--verbose
