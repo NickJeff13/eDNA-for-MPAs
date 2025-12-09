7f832a045b6bb2491936dcc6a2c5dff4e1318eab
NickJeff13 <nick.w.jeffery3@gmail.com>
2025-12-09 14:05:16 -0400

    merging ASV tables with fasta sequences

--- code/R_Analyses/merge_fastas_and_asvTables.R
+++ code/R_Analyses/merge_fastas_and_asvTables.R
@@ -36,3 +36,36 @@ fas <- data.frame(ASV = ids, sequence = seqs)
 sab24.with.seqs <- left_join(sab24.12s.filt, fas, by=c("OTU.ID"="ASV"))
 
 write.csv(x = sab24.with.seqs, file = "data/2024Perley/12S/SAB2024_ASVs_WithSEQS.csv", row.names = F, quote=F)
+
+
+## COI 
+#Redoing this filtration step from filters_ASVs.R but keeping OTU.ID
+sab24.coi.filt <- filter_low_reads(sab24.coi.merge %>% 
+                                     select(-c(V2:V11,V13,V22, V25, V28)) %>%
+                                     filter(V29>0.979, V12 %in% c("Arthropoda","Platyhelminthes","Chordata","Annelida","Mollusca","Nematoda","Rhodophyta",
+                                                                  "Gastrotricha","Chlorophyta","Echinodermata","Brachiopoda","Porifera","Cnidaria",
+                                                                  "Nemertea","Haptophyta","Hemichordata","Bryozoa","Ctenophora_comb_jellies","Tardigrada",
+                                                                  "Rotifera", "Chaetognatha","Kinorhyncha","Acanthocephala_thorny-headed_worms")) %>%
+                                     rename(Phylum=V12, Class=V15, Species=V27) %>% 
+                                     as.data.frame())
+
+#Load the fasta file which is a single column of ASV names followed by their DNA sequence.
+# We'll need to split this into 2 columns to make it easy to merge
+sab24.coi.fasta <- readLines("data/2024Perley/COI/dna-sequences.fasta")
+
+
+# extract IDs (remove ">")
+ids <- sab24.coi.fasta[grepl("^>", sab24.coi.fasta)] |> gsub("^>", "",x=_)
+
+# extract sequences (lines not starting with ">")
+seqs <- sab24.coi.fasta[!grepl("^>", sab24.coi.fasta)]
+
+# combine into a data frame
+fas <- data.frame(ASV = ids, sequence = seqs)
+
+
+#Now merge with filtered ASV file
+
+sab24.COIwith.seqs <- left_join(sab24.coi.filt, fas, by=c("OTU.ID"="ASV"))
+
+write.csv(x = sab24.COIwith.seqs, file = "data/2024Perley/COI/SAB2024_COIASVs_WithSEQS.csv", row.names = F, quote=F)
